49bc911fb11d873273d3ada01d862e39b912a444
icaroccaetano <icaroccaetano@gmail.com>
2024-04-06 00:02:23 -0300

    Adicionar arquivos de projeto.

---
+++ ConvesorMoedas.Dominio/ConvesorMoedas.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ConvesorMoedas.Dominio
+{
+    public class ConvesorMoedas
+    {
+        CultureInfo real = new CultureInfo("BRA", false);
+        CultureInfo dolar = new CultureInfo("USA", false);
+        CultureInfo libra = new CultureInfo("ING", false);
+        CultureInfo iene = new CultureInfo("CHI", false);
+        CultureInfo euro = new CultureInfo("EUR", false);
+
+        void inicializaMoedas ()
+        {
+            real.NumberFormat.CurrencySymbol = "R$";
+            dolar.NumberFormat.CurrencySymbol = "$";
+            libra.NumberFormat.CurrencySymbol = "€";
+            iene.NumberFormat.CurrencySymbol = "¥";
+            euro.NumberFormat.CurrencySymbol = "£";
+        }
+    }
+}
